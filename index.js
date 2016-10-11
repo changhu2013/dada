96d55e9f3910c42b7eb344484aa9ecf19c028179
changhu <o52tiger@gmail.com>
2016-10-11 06:10:39 +0000

    gpio

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+var gpio = require('gpio');
 
+var gpio4 = gpio.export(4, {
+    direction:'out',
+    interval :200,
+    ready : function(){
+        console.log('ok');
+    }
+});
+
+console.log(gpio4);
+
+gpio4.set();
 
 console.log("hello world");
